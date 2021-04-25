c763e174aad9f74f6fda8e6dc6d8474aee9009ea
fran <frano4240>
2021-04-24 21:44:48 -0300

    Room DB Instance

---
+++ app/src/main/java/com/androiddevs/ucanbit/ui/db/CoinDatabase.kt
@@ -0,0 +1,39 @@
+package com.androiddevs.ucanbit.ui.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [CoinDatabase::class],
+    version = 1
+)
+abstract class CoinDatabase : RoomDatabase() {
+
+
+    abstract fun getCoinDao(): CoinDao
+
+    companion object{
+
+        @Volatile
+        private var instance: CoinDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: createDatabase(context).also{ instance = it}
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                CoinDatabase::class.java,
+                "coin_db.db"
+            ).build()
+
+
+
+    }
+
+
+}
